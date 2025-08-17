f9de314681cff4e15b08282b169917f654330489
Angela Sekar Widelia <117893433+widiwidelia@users.noreply.github.com>
2025-08-17 22:14:12 +0700

    konversiSuhu

---
+++ coba2/coba2/Form1.Designer.vb
@@ -0,0 +1,115 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.lblCelcius = New System.Windows.Forms.Label()
+        Me.txtCelcius = New System.Windows.Forms.TextBox()
+        Me.btnConvert = New System.Windows.Forms.Button()
+        Me.btnKeluar = New System.Windows.Forms.Button()
+        Me.txtFahrenheit = New System.Windows.Forms.TextBox()
+        Me.txtReamur = New System.Windows.Forms.TextBox()
+        Me.txtKelvin = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'lblCelcius
+        '
+        Me.lblCelcius.AutoSize = True
+        Me.lblCelcius.Location = New System.Drawing.Point(46, 36)
+        Me.lblCelcius.Name = "lblCelcius"
+        Me.lblCelcius.Size = New System.Drawing.Size(51, 16)
+        Me.lblCelcius.TabIndex = 0
+        Me.lblCelcius.Text = "Celcius"
+        '
+        'txtCelcius
+        '
+        Me.txtCelcius.Location = New System.Drawing.Point(103, 33)
+        Me.txtCelcius.Name = "txtCelcius"
+        Me.txtCelcius.Size = New System.Drawing.Size(100, 22)
+        Me.txtCelcius.TabIndex = 1
+        '
+        'btnConvert
+        '
+        Me.btnConvert.Location = New System.Drawing.Point(209, 32)
+        Me.btnConvert.Name = "btnConvert"
+        Me.btnConvert.Size = New System.Drawing.Size(75, 23)
+        Me.btnConvert.TabIndex = 2
+        Me.btnConvert.Text = "Convert"
+        Me.btnConvert.UseVisualStyleBackColor = True
+        '
+        'btnKeluar
+        '
+        Me.btnKeluar.Location = New System.Drawing.Point(290, 33)
+        Me.btnKeluar.Name = "btnKeluar"
+        Me.btnKeluar.Size = New System.Drawing.Size(75, 23)
+        Me.btnKeluar.TabIndex = 3
+        Me.btnKeluar.Text = "Keluar"
+        Me.btnKeluar.UseVisualStyleBackColor = True
+        '
+        'txtFahrenheit
+        '
+        Me.txtFahrenheit.Location = New System.Drawing.Point(43, 126)
+        Me.txtFahrenheit.Name = "txtFahrenheit"
+        Me.txtFahrenheit.Size = New System.Drawing.Size(100, 22)
+        Me.txtFahrenheit.TabIndex = 4
+        '
+        'txtReamur
+        '
+        Me.txtReamur.Location = New System.Drawing.Point(158, 126)
+        Me.txtReamur.Name = "txtReamur"
+        Me.txtReamur.Size = New System.Drawing.Size(100, 22)
+        Me.txtReamur.TabIndex = 5
+        '
+        'txtKelvin
+        '
+        Me.txtKelvin.Location = New System.Drawing.Point(274, 126)
+        Me.txtKelvin.Name = "txtKelvin"
+        Me.txtKelvin.Size = New System.Drawing.Size(100, 22)
+        Me.txtKelvin.TabIndex = 6
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(431, 190)
+        Me.Controls.Add(Me.txtKelvin)
+        Me.Controls.Add(Me.txtReamur)
+        Me.Controls.Add(Me.txtFahrenheit)
+        Me.Controls.Add(Me.btnKeluar)
+        Me.Controls.Add(Me.btnConvert)
+        Me.Controls.Add(Me.txtCelcius)
+        Me.Controls.Add(Me.lblCelcius)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents lblCelcius As Label
+    Friend WithEvents txtCelcius As TextBox
+    Friend WithEvents btnConvert As Button
+    Friend WithEvents btnKeluar As Button
+    Friend WithEvents txtFahrenheit As TextBox
+    Friend WithEvents txtReamur As TextBox
+    Friend WithEvents txtKelvin As TextBox
+End Class
---
+++ coba2/coba2/Form1.vb
@@ -0,0 +1,49 @@
+﻿Public Class Form1
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles lblCelcius.Click
+
+    End Sub
+
+    Private Sub btnConvert_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnConvert.Click
+        Dim celcius, fahrenheit, reamur, kelvin As Double
+        celcius = Val(txtCelcius.Text)
+        fahrenheit = (9 / 5) * celcius + 32
+        reamur = (4 / 5) * celcius
+        kelvin = celcius + 273
+
+        'ini kalau tampilin pake MsgBox
+        'MsgBox("Fahrenheit = " & fahrenheit & vbCrLf & "Reamur = " & reamur & vbCrLf & "Kelvin = " & kelvin)
+
+        'ini kalau tampilin pake label
+        'lblFahrenheit.Text = CStr(fahrenheit)
+        'lblReamur.Text = CStr(reamur)
+        'lblKelvin.Text = CStr(kelvin)
+
+        txtFahrenheit.Text = CStr(fahrenheit)
+        txtReamur.Text = CStr(reamur)
+        txtKelvin.Text = CStr(kelvin)
+    End Sub
+
+    Private Sub btnKeluar_Click(sender As Object, e As EventArgs) Handles btnKeluar.Click
+        Me.Close()
+    End Sub
+
+    Private Sub Label1_Click_1(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub Label1_Click_2(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub txtCelcius_TextChanged(sender As Object, e As EventArgs) Handles txtCelcius.TextChanged
+
+    End Sub
+
+    Private Sub TextBox2_TextChanged(sender As Object, e As EventArgs) Handles txtReamur.TextChanged
+
+    End Sub
+
+    Private Sub txtFahrenheit_TextChanged(sender As Object, e As EventArgs) Handles txtFahrenheit.TextChanged
+
+    End Sub
+End Class
